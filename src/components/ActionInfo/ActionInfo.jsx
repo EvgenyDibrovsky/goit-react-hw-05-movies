e60233a637986e770644f63bf600571ce997b341
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2022-12-27 20:52:59 +0100

    Update ActionInfo.jsx

--- src/components/ActionInfo/ActionInfo.jsx
+++ src/components/ActionInfo/ActionInfo.jsx
@@ -5,12 +5,12 @@ export const ActionInfo = () => {
   return (
     <ul>
       <li>
-        <NavLink to="cast" state={{ from: location.state.from }}>
+        <NavLink to="cast" state={{ from: location?.state.from }}>
           Cast
         </NavLink>
       </li>
       <li>
-        <NavLink to="reviews" state={{ from: location.state.from }}>
+        <NavLink to="reviews" state={{ from: location?.state.from }}>
           Reviews
         </NavLink>
       </li>
