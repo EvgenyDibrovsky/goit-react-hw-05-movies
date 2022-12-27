8f6002c43fddb2b054c14ec08f1a7d410f20cac5
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2022-12-27 20:07:49 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import { SharedLayout } from 'Layout/SharedLayout';
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('pages/MovieDetails/Cast/Cast'));
-const Reviews = lazy(() => import('pages/MovieDetails/Reviews/Reviews'));
+const Cast = lazy(() => import('pages/Cast/Cast'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
 
 export const App = () => {
   return (
     <Suspense fallback={<div>Loading page...</div>}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Home />} />   
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
